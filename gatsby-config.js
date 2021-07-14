65fdfb142c22b4541953bb70503b3ee05f75a5d8
asm0dey <pavel.finkelshtein@gmail.com>
2021-07-14 16:32:49 +0300

    Initial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,58 @@
+const feedOptions = {
+  title: "De or Die: Talks",
+  description: "description",
+  feed_url: "https://talks.deordie.org/feed.xml",
+  site_url: "https://talks.deordie.org",
+  image_url: "http://example.com/icon.png",
+  managingEditor: "Pasha Finkelshteyn",
+  webMaster: "Pasha Finkelshteyn",
+  copyright: "2021 DE or Die",
+  language: "ru",
+  categories: ["IT", "Data Enginerring"],
+  pubDate: "May 20, 2019 04:00:00 GMT",
+  ttl: "60",
+  custom_namespaces: {
+    itunes: "http://www.itunes.com/dtds/podcast-1.0.dtd",
+  },
+  custom_elements: [
+    { "itunes:subtitle": "Data enginerring from Data Engneers" },
+    { "itunes:author": "DE or Die" },
+    {
+      "itunes:summary":
+        "Here we are discussing topics and news somehow related to the data engineering",
+    },
+    {
+      "itunes:owner": [
+        { "itunes:name": "Pasha Finkelshteyn" },
+        { "itunes:email": "pavel.finkelshten@gmail.com" },
+      ],
+    },
+    {
+      "itunes:image": {
+        _attr: {
+          href: "http://example.com/podcasts/everything/AllAboutEverything.jpg",
+        },
+      },
+    },
+    {
+      "itunes:category": [
+        {
+          _attr: {
+            text: "Technology",
+          },
+        },
+        {
+          "itunes:category": {
+            _attr: {
+              text: "Gadgets",
+            },
+          },
+        },
+      ],
+    },
+  ],
+}
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
@@ -32,6 +87,16 @@ module.exports = {
       options: {
         plugins: [
           {
+            resolve: "gatsby-remark-audio",
+            options: {
+              preload: "auto",
+              loop: false,
+              controls: true,
+              muted: false,
+              autoplay: false,
+            },
+          },
+          {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 630,
@@ -57,56 +122,62 @@ module.exports = {
     //     trackingId: `ADD YOUR TRACKING ID HERE`,
     //   },
     // },
+    //{
+    //resolve: `gatsby-plugin-feed`,
+    //options: {
+    //query: `
+    //{
+    //site {
+    //siteMetadata {
+    //title
+    //description
+    //siteUrl
+    //site_url: siteUrl
+    //}
+    //}
+    //}
+    //`,
+    //feeds: [
+    //{
+    //serialize: ({ query: { site, allMarkdownRemark } }) => {
+    //return allMarkdownRemark.nodes.map(node => {
+    //return Object.assign({}, node.frontmatter, {
+    //description: node.excerpt,
+    //date: node.frontmatter.date,
+    //url: site.siteMetadata.siteUrl + node.fields.slug,
+    //guid: site.siteMetadata.siteUrl + node.fields.slug,
+    //custom_elements: [{ "content:encoded": node.html }],
+    //})
+    //})
+    //},
+    //query: `
+    //{
+    //allMarkdownRemark(
+    //sort: { order: DESC, fields: [frontmatter___date] },
+    //) {
+    //nodes {
+    //excerpt
+    //html
+    //fields {
+    //slug
+    //}
+    //frontmatter {
+    //title
+    //date
+    //}
+    //}
+    //}
+    //}
+    //`,
+    //output: "/rss.xml",
+    //},
+    //],
+    //},
+    //},
     {
-      resolve: `gatsby-plugin-feed`,
+      resolve: `gatsby-plugin-podcast-feed`,
       options: {
-        query: `
-          {
-            site {
-              siteMetadata {
-                title
-                description
-                siteUrl
-                site_url: siteUrl
-              }
-            }
-          }
-        `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.nodes.map(node => {
-                return Object.assign({}, node.frontmatter, {
-                  description: node.excerpt,
-                  date: node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + node.fields.slug,
-                  custom_elements: [{ "content:encoded": node.html }],
-                })
-              })
-            },
-            query: `
-              {
-                allMarkdownRemark(
-                  sort: { order: DESC, fields: [frontmatter___date] },
-                ) {
-                  nodes {
-                    excerpt
-                    html
-                    fields {
-                      slug
-                    }
-                    frontmatter {
-                      title
-                      date
-                    }
-                  }
-                }
-              }
-            `,
-            output: "/rss.xml",
-          },
-        ],
+        feedOptions,
       },
     },
     {
