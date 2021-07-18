a7569be33789a0eae90d84eaf7008a1e5df03ffa
asm0dey <pavel.finkelshtein@gmail.com>
2021-07-18 20:23:16 +0300

    Fixes link to the episode in RSS

--- plugins/gatsby-mdx-podcast-feed/gatsby-node.js
+++ plugins/gatsby-mdx-podcast-feed/gatsby-node.js
@@ -1,14 +1,14 @@
-const fs = require('fs-extra');
-const path = require('path');
-const RSS = require('rss');
+const fs = require("fs-extra")
+const path = require("path")
+const RSS = require("rss")
 
 const wrapper = promise =>
   promise.then(result => {
     if (result.errors) {
-      throw result.errors;
+      throw result.errors
     }
-    return result;
-  });
+    return result
+  })
 
 exports.onPostBuild = async ({ graphql }, pluginOptions) => {
   const result = await wrapper(
@@ -28,61 +28,64 @@ exports.onPostBuild = async ({ graphql }, pluginOptions) => {
                 date
                 size
               }
+              fields {
+                slug
+              }
             }
           }
         }
       }
     `)
-  );
+  )
 
-  const { feedOptions } = pluginOptions || {};
+  const { feedOptions } = pluginOptions || {}
 
-  const episodes = result.data.allMdx.edges;
+  const episodes = result.data.allMdx.edges
 
-  const feed = new RSS(feedOptions);
+  const feed = new RSS(feedOptions)
 
   episodes.forEach(edge => {
-    const { html, excerpt, id } = edge.node;
-    const { title, number, date, url, categories, size } = edge.node.frontmatter;
-
+    const { html, excerpt, id } = edge.node
+    const { title, number, date, url, categories, size } = edge.node.frontmatter
+    const { slug } = edge.node.fields
     feed.item({
       guid: id,
       title,
-      url,
+      url: feedOptions.site_url + slug,
       description: excerpt,
       categories,
       author: feedOptions.managingEditor,
       date,
       custom_elements: [
-        { 'content:encoded': html },
+        { "content:encoded": html },
         { pubDate: date },
-        { 'itunes:explicit': 'no' },
-        { 'itunes:episodeType': 'full' },
-        { 'itunes:title': title },
-        { 'itunes:episode': number },
-        { 'itunes:summary': excerpt },
-        { 'itunes:author': feedOptions.managingEditor },
+        { "itunes:explicit": "no" },
+        { "itunes:episodeType": "full" },
+        { "itunes:title": title },
+        { "itunes:episode": number },
+        { "itunes:summary": excerpt },
+        { "itunes:author": feedOptions.managingEditor },
         {
-          'itunes:image': {
+          "itunes:image": {
             _attr: {
-              href: feedOptions.image_url,
-            },
-          },
-        },
+              href: feedOptions.image_url
+            }
+          }
+        }
       ],
       enclosure: {
         url,
         size,
-        type: 'audio/mpeg'
-      },
-    });
-  });
+        type: "audio/mpeg"
+      }
+    })
+  })
 
-  const publicPath = `./public`;
-  const outputPath = path.join(publicPath, '/rss.xml');
-  const outputDir = path.dirname(outputPath);
+  const publicPath = `./public`
+  const outputPath = path.join(publicPath, "/rss.xml")
+  const outputDir = path.dirname(outputPath)
   if (!(await fs.exists(outputDir))) {
-    await fs.mkdirp(outputDir);
+    await fs.mkdirp(outputDir)
   }
-  await fs.writeFile(outputPath, feed.xml());
-};
+  await fs.writeFile(outputPath, feed.xml())
+}
