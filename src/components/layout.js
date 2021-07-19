144db947f2095a4adf0490f65a7823c588dd3245
asm0dey <pavel.finkelshtein@gmail.com>
2021-07-19 13:13:32 +0300

    Adds links to subscribe

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 import { MDXProvider } from "@mdx-js/react"
 import "react-jinke-music-player/assets/index.css"
 import loadable from "@loadable/component"
+import { StaticImage } from "gatsby-plugin-image"
 
 const ReactJkMusicPlayer = loadable(() => import("react-jinke-music-player"))
 const shortcodes = { ReactJkMusicPlayer }
@@ -28,6 +29,76 @@ const Layout = ({ location, title, children }) => {
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
       <header className="global-header">{header}</header>
+      <p>
+        <Link to="/rss.xml" title="RSS">
+          <StaticImage
+            formats={["AUTO", "WEBP", "AVIF"]}
+            src="../images/rss.svg"
+            width={50}
+            height={50}
+            quality={95}
+            alt="RSS"
+          />
+        </Link>
+        {` `}
+        <a
+          target="_blank"
+          href="https://podcasts.apple.com/us/podcast/de-or-die-talks/id1577026407"
+          title="Apple Podcasts"
+          rel="noreferrer"
+        >
+          <StaticImage
+            formats={["AUTO", "WEBP", "AVIF"]}
+            src="../images/apple-podcast.svg"
+            width={50}
+            height={50}
+            quality={95}
+            alt="Apple Podcasts"
+          />
+        </a>
+        {` `}
+        <StaticImage
+          formats={["AUTO", "WEBP", "AVIF"]}
+          src="../images/google-podcast.svg"
+          width={50}
+          height={50}
+          quality={95}
+          alt="Google Podcasts (pending)"
+          title="Google Podcasts (pending)"
+        />
+        {` `}
+        <a
+          target="_blank"
+          href="https://www.stitcher.com/show/643004"
+          title="Stitcher (pending)"
+          rel="noreferrer"
+        >
+          <StaticImage
+            formats={["AUTO", "WEBP", "AVIF"]}
+            src="../images/stitcher.png"
+            width={50}
+            height={50}
+            quality={95}
+            alt="Stitcher (pending)"
+          />
+        </a>
+        {` `}
+        <a
+          target="_blank"
+          href="https://www.subscribeonandroid.com/talks.deordie.org/rss.xml"
+          title="Subscribe on Android"
+          rel="noreferrer"
+        >
+          <StaticImage
+            formats={["AUTO", "WEBP", "AVIF"]}
+            src="../images/android.png"
+            width={50}
+            height={50}
+            quality={95}
+            alt="Subscribe on Android"
+          />
+        </a>
+      </p>
       <MDXProvider components={shortcodes}>{children}</MDXProvider>
       <footer>
         © {new Date().getFullYear()}, Built with
